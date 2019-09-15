5e76b8ab764a0c64cd03a1af7461ed6713d50027
Daniel I <danielionita88@yahoo.com>
2019-09-15 22:00:59 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -3,12 +3,17 @@ def mario
   puts phrase
 end
 
+
+
 def toadstool
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
 
+
 def link 
   suggestion = "It's Dangerous To Go Alone! Take This."
   puts suggestion
 end
+
+
